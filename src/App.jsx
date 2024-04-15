d6872ac037be1134149c158e60d1910c2b77bd46
A-Hamdi1 <akramhamdi299@gmail.com>
2024-04-15 20:21:46 +0100

    15-04-2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Login from "./pages/login";
 import Register from "./pages/register";
 import {BrowserRouter, Routes, Route, Navigate,} from "react-router-dom";
